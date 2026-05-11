884ec2a86f99a47d9f8394e6f7ef9b5342cd0195
Levi Amorim <levileme2006@hotmail.com>
2026-05-11 18:36:02 -0300

    Add file organizer script
    Initial commit of a Python script for organizing files by type.

---
+++ organizer.py
@@ -0,0 +1,15 @@
+# File Organizer
+
+Python script that automatically organizes files into folders based on file type.
+
+## Features
+- Organizes images
+- Organizes documents
+- Organizes videos
+- Organizes music files
+
+## Technologies
+- Python
+
+## Goal
+Practice automation and file management with Python.
